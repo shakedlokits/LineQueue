c3f4ec5ac0c9691455f5d5bf434fa6117c082030
Ophir Sheriff <op.sheriff@gmail.com>
2017-09-30 20:20:39 +0300

    finalized design for confirmation page

--- lineQueueClient/src/components/Confirmation/Confirmation.js
+++ lineQueueClient/src/components/Confirmation/Confirmation.js
@@ -2,29 +2,30 @@ import React from 'react'
 import {Text, View} from 'react-native';
 import Header from '../Header/Header'
 import globals from '../../styles/globals.style'
+import confirmationStyle from './Confirmation.style'
 
 const Confirmation = (props) =>
 {
   return (
     <View style={globals.sceneContainer}>
       <Header/>
-      <View style={globals.ContentContainer}>
+      <View style={globals.contentContainer}>
+        <View style={confirmationStyle.textContainer}>
+          <Text style={[globals.textLarge, globals.textBold]}>
+            תודה על שהשתמשת
+            {"\n"}
+            במערכת הזמנת התורים
+          </Text>
+        </View>
+        <View style={confirmationStyle.footerContainer}>
+          <Text style={globals.textRegular}>
+            היה שלום <Text style={globals.textBold}>{props.fullName},</Text>
+          </Text>
 
-        <Text style={globals.textRegular}>
-          תודה על שהשתמשת
-          במערכת הזמנת התורים
-        </Text>
-
-
-        <Text style={globals.textRegular}>
-          היה שלום
-        </Text>
-        <Text style={[globals.textRegular, globals.textBold]}>
-          {props.fullName},
-        </Text>
-        <Text style={globals.textRegular}>
-          ותודה על הדגים
-        </Text>
+          <Text style={globals.textRegular}>
+            ותודה על הדגים
+          </Text>
+        </View>
 
       </View>
     </View>
